ec004e38c1cddde556305664adb5bef687fc0232
Ivan Berestianyi <berestvania@gmail.com>
2024-11-20 15:06:19 +0200

    add Documents model

--- src/documents/models.py
+++ src/documents/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Documents(models.Model):
+    path = models.CharField(blank=True, null=True, max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    contract = models.ForeignKey(
+        'contracts.Contracts',
+        on_delete=models.CASCADE,
+        blank=True,
+        null=True
+    )
+
+    def __str__(self):
+        return self.path
