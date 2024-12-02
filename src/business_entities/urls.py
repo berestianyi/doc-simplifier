180a218522ff52cc6e2f39fff562cb6771b7e16f
Ivan Berestianyi <berestvania@gmail.com>
2024-12-02 12:58:41 +0200

    made url for create, list, page

--- src/business_entities/urls.py
+++ src/business_entities/urls.py
@@ -5,4 +5,6 @@ from business_entities import views
 urlpatterns = [
     path('', views.home_page, name='home_page'),
     path('business-entities', views.business_entities, name='business_entities'),
+    path('business-entity', views.business_entity, name='business_entity'),
+    path('create-business-entity', views.create_business_entity, name='create_business_entity'),
 ]
