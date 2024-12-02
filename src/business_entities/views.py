4fcbacc84bd2e081852367d5619432438c86b764
Ivan Berestianyi <berestvania@gmail.com>
2024-12-02 12:59:14 +0200

    made view for creation of business_entity

--- src/business_entities/views.py
+++ src/business_entities/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+from business_entities.forms import BusinessEntitiesForm
+from business_entities.models import BusinessEntities
 
 
 def home_page(request):
@@ -6,4 +9,49 @@ def home_page(request):
 
 
 def business_entities(request):
-    return render(request, 'business_entities.html')
+    return render(request, 'business_entity/list.html')
+
+
+def business_entity(request):
+    return render(request, 'business_entity/page.html')
+
+
+def create_business_entity(request):
+    if request.method == 'POST':
+        data = request.POST
+        edrpou = data.get('edrpou', '').strip()
+        form = BusinessEntitiesForm(request.POST, request.FILES)
+        if form.is_valid():
+            if len(edrpou) == 8:
+                business_entity_type = BusinessEntities.BusinessEntitiesEnum.FOP
+                short_name = full_name = director_name = data.get('fop_name', '')
+            elif len(edrpou) == 10:
+                business_entity_type = BusinessEntities.BusinessEntitiesEnum.TOV
+                short_name = full_name = data.get('company_name', '')
+                director_name = data.get('director_name', '')
+            else:
+                return render(request, 'business_entity/create.html', {'error': 'Неправильний код ЕДРПОУ'})
+
+            business_entity_instance = BusinessEntities(
+                business_entity=business_entity_type,
+                edrpou=edrpou,
+                short_name=short_name,
+                full_name=full_name,
+                director_name=director_name,
+                address=data.get('address', ''),
+                email=data.get('email', ''),
+                phone=data.get('phone', ''),
+                iban=data.get('iban', ''),
+            )
+
+            business_entity_instance.save()
+            return redirect('business_entities')
+    else:
+        form = BusinessEntitiesForm()
+
+    return render(
+        request,
+        'business_entity/create.html',
+        {'form': form}
+    )
+
