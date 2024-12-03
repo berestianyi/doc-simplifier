d05d6e848937a6adcec3d7a2a196005316b86245
Ivan Berestianyi <berestvania@gmail.com>
2024-12-04 00:30:26 +0200

    changed js file

--- src/static/js/business_entities/create.js
+++ src/static/js/business_entities/create.js
@@ -4,30 +4,25 @@ document.addEventListener('DOMContentLoaded', function() {
     const submitButton = document.getElementById('submitButton')
 
     const inputEdrpou = document.querySelector('div #id_edrpou');
-    const inputFopName = document.querySelector('div #id_fop_name');
-    const inputDirectorName= document.querySelector('div #id_director_name');
-    const inputCompanyName= document.querySelector('div #id_company_name');
+    const directorName= document.querySelector('div #id_director_name').parentElement;
+    const companyName= document.querySelector('div #id_company_name').parentElement;
+    const labelDirectorName = directorName.querySelector('label');
 
     function toggleForms() {
         inputEdrpou.value = inputEdrpou.value.substring(0, 8);
-        inputFopName.value = "";
-        inputDirectorName.value = "";
-        inputCompanyName.value = "";
 
         if (tovChoice.checked) {
             submitButton.textContent = 'Створити ТОВ'
             inputEdrpou.setAttribute('minlength', '10')
             inputEdrpou.setAttribute('maxlength', '10')
-            inputFopName.parentElement.style.display = 'none'
-            inputDirectorName.parentElement.style.display = 'block';
-            inputCompanyName.parentElement.style.display = 'block';
+            labelDirectorName.textContent = 'Імʼя директора ТОВа (ПІБ)'
+            companyName.style.display = 'block';
         } else if (fopChoice.checked) {
             submitButton.textContent = 'Створити ФОП'
             inputEdrpou.setAttribute('minlength', '8')
             inputEdrpou.setAttribute('maxlength', '8')
-            inputFopName.parentElement.style.display = 'block'
-            inputDirectorName.parentElement.style.display = 'none';
-            inputCompanyName.parentElement.style.display = 'none';
+            labelDirectorName.textContent = 'Імʼя ФОПа (ПІБ)'
+            companyName.style.display = 'none';
         }
     }
 
