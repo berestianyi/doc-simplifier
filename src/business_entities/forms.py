461263c1ba00a1d4c91900776a8688883b1a8a14
Ivan Berestianyi <berestvania@gmail.com>
2024-12-04 00:29:07 +0200

    changed forms.Form to forms.ModelForm

--- src/business_entities/forms.py
+++ src/business_entities/forms.py
@@ -1,69 +1,80 @@
 from django import forms
 from django.core.exceptions import ValidationError
 from django.utils.translation import gettext_lazy as _
+from .models import BusinessEntities
 
 
-def validate_only_digits(value):
-
+def validate_edrpou(value):
     if not value.isdigit():
         raise ValidationError(_('Це поле повинно містити лише цифри.'), code='invalid')
 
+    if BusinessEntities.objects.filter(edrpou=value).exists():
+        raise ValidationError(_("Цей код ЄДРПОУ вже існує."), code='invalid')
 
-class BusinessEntitiesForm(forms.Form):
+
+class BusinessEntitiesForm(forms.ModelForm):
     edrpou = forms.CharField(
         required=False,
         label='ЄДРПОУ',
-        max_length=8,
+        max_length=10,
         min_length=8,
-        widget=forms.TextInput(attrs={'placeholder': '12345678'}),
-        validators=[validate_only_digits]
+        validators=[validate_edrpou],
+        widget=forms.TextInput(attrs={
+            'placeholder': '12345678',
+            'class': 'form-control',
+        })
     )
-    fop_name = forms.CharField(
-        required=False,
-        label='Імʼя ФОПа (ПІБ)',
-        max_length=150,
-        widget=forms.TextInput(attrs={'placeholder': 'Іванов Іван Іванович'})
-    )
-    director_name = forms.CharField(
-        required=False,
-        label='Імʼя директора ТОВа (ПІБ)',
-        max_length=150,
-        widget=forms.TextInput(attrs={'placeholder': 'Іванов Іван Іванович'})
-    )
-    company_name = forms.CharField(
-        required=False,
-        label='Назва ТОВа',
-        max_length=200,
-        widget=forms.TextInput(attrs={'placeholder': 'Google LLC'})
-    )
-    address = forms.CharField(
-        required=False,
-        label='Адреса',
-        max_length=200,
-        widget=forms.TextInput(attrs={'placeholder': 'УкраЇна, вул. Хрещатик, 1'})
-    )
-    phone = forms.CharField(
-        required=False,
-        label='Телефон',
-        max_length=100,
-        widget=forms.TextInput(attrs={'placeholder': '+3809901234567'})
-    )
-    email = forms.EmailField(
-        required=False,
-        label='Email',
-        widget=forms.EmailInput(attrs={'placeholder': 'example@gmail.com'})
-    )
-    iban = forms.CharField(
-        required=False,
-        label='IBAN',
-        max_length=34,
-        widget=forms.TextInput(attrs={'placeholder': 'UAXXXXXXXXXXXXXXXXXXXXXXXXXXX'})
-    )
+
+    class Meta:
+        model = BusinessEntities
+        fields = [
+            'edrpou',
+            'director_name',
+            'company_name',
+            'address',
+            'phone',
+            'email',
+            'iban',
+        ]
+        labels = {
+            'director_name': 'Імʼя директора ТОВа (ПІБ)',
+            'company_name': 'Назва ТОВа',
+            'address': 'Адреса',
+            'phone': 'Телефон',
+            'email': 'Email',
+            'iban': 'IBAN',
+        }
+        widgets = {
+            'director_name': forms.TextInput(attrs={
+                'placeholder': 'Іванов Іван Іванович',
+                'class': 'form-control',
+            }),
+            'company_name': forms.TextInput(attrs={
+                'placeholder': 'Google LLC',
+                'class': 'form-control',
+            }),
+            'address': forms.TextInput(attrs={
+                'placeholder': 'Україна, вул. Хрещатик, 1',
+                'class': 'form-control',
+            }),
+            'phone': forms.TextInput(attrs={
+                'placeholder': '+3809901234567',
+                'class': 'form-control',
+            }),
+            'email': forms.EmailInput(attrs={
+                'placeholder': 'example@gmail.com',
+                'class': 'form-control',
+            }),
+            'iban': forms.TextInput(attrs={
+                'placeholder': 'UAXXXXXXXXXXXXXXXXXXXXXXXXXXX',
+                'class': 'form-control',
+            }),
+        }
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
+        # Ensure all fields have 'form-control' class for Bootstrap styling
         for field_name, field in self.fields.items():
-            field.widget.attrs.update({
-                'class': 'form-control',
-            })
+            if 'class' not in field.widget.attrs:
+                field.widget.attrs['class'] = 'form-control'
