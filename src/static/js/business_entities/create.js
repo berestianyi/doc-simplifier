d19f828ab22bdb05d8abf67a0766a6bf971543ce
Ivan Berestianyi <berestvania@gmail.com>
2024-12-05 14:38:05 +0200

    modified the js according to the changes

--- src/static/js/business_entities/create.js
+++ src/static/js/business_entities/create.js
@@ -1,23 +1,23 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const tovChoice = document.getElementById('tovChoice');
-    const fopChoice = document.getElementById('fopChoice');
+    const tovChoice = document.getElementById('id_business_entity_0');
+    const fopChoice = document.getElementById('id_business_entity_1');
     const submitButton = document.getElementById('submitButton')
 
     const inputEdrpou = document.querySelector('div #id_edrpou');
     const directorName= document.querySelector('div #id_director_name').parentElement;
-    const companyName= document.querySelector('div #id_company_name').parentElement;
-    const labelDirectorName = directorName.querySelector('label');
+    const companyName= document.getElementById('id_company_name_div')
+    const labelDirectorName = directorName.parentElement.querySelector('label');
 
     function toggleForms() {
-        inputEdrpou.value = inputEdrpou.value.substring(0, 8);
 
         if (tovChoice.checked) {
             submitButton.textContent = 'Створити ТОВ'
             inputEdrpou.setAttribute('minlength', '10')
             inputEdrpou.setAttribute('maxlength', '10')
-            labelDirectorName.textContent = 'Імʼя директора ТОВа (ПІБ)'
+            labelDirectorName.textContent = 'Імʼя директора (ПІБ)'
             companyName.style.display = 'block';
         } else if (fopChoice.checked) {
+            inputEdrpou.value = inputEdrpou.value.substring(0, 8);
             submitButton.textContent = 'Створити ФОП'
             inputEdrpou.setAttribute('minlength', '8')
             inputEdrpou.setAttribute('maxlength', '8')
