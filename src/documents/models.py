8f4cc6a61b0dfb8a8f40d19cd6462ab6fd997674
Ivan Berestianyi <berestvania@gmail.com>
2025-01-04 15:41:31 +0200

    changed Charfield to FileField in Documents model

--- src/documents/models.py
+++ src/documents/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Documents(models.Model):
-    path = models.CharField(blank=True, null=True, max_length=100)
+    path = models.FileField(upload_to='uploads/', blank=True, null=True, max_length=256)
     created_at = models.DateTimeField(auto_now_add=True)
     contract = models.ForeignKey(
         'contracts.Contracts',
