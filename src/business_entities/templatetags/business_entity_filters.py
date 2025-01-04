5513a6f4db1a993824cca7f7fd799aca147e38f0
Ivan Berestianyi <berestvania@gmail.com>
2025-01-04 13:20:06 +0200

    fixed business_entity templatetags

--- src/business_entities/templatetags/business_entity_filters.py
+++ src/business_entities/templatetags/business_entity_filters.py
@@ -4,22 +4,18 @@ register = template.Library()
 
 
 @register.filter
-def make_short_name(company_name, edrpou):
-    print(edrpou)
-    print(company_name)
-    if len(edrpou) == 8:
-        return f"ФОП {company_name}"
-    elif len(edrpou) == 10:
+def make_short_name(company_name, director_name):
+    if company_name:
         return f"ТОВ «{company_name}»"
-    else:
-        return company_name
+
+    return f"ФОП {director_name}"
 
 
 @register.filter
 def make_full_name(company_name, edrpou):
-    if len(edrpou) == 8:
+    if len(edrpou) == 10:
         return f"Фізична особа-підприємець {company_name}"
-    elif len(edrpou) == 10:
+    elif len(edrpou) == 8:
         return f"Товариство з обмеженою відповідальністю «{company_name}»"
     else:
         return company_name
