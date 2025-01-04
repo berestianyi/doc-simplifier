05452dd46dd3633ea0c7dbca0bc16d1d929e723b
Ivan Berestianyi <berestvania@gmail.com>
2025-01-04 13:36:30 +0200

    changed parameter on_delete from CASCADE to SET_NULL

--- src/documents/models.py
+++ src/documents/models.py
@@ -6,7 +6,7 @@ class Documents(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     contract = models.ForeignKey(
         'contracts.Contracts',
-        on_delete=models.CASCADE,
+        on_delete=models.SET_NULL,
         blank=True,
         null=True
     )
