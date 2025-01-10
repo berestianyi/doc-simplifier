7da4eebf0b21bd63fb25bd65c9fe8ce931b0efca
Ivan Berestianyi <berestvania@gmail.com>
2025-01-10 14:44:10 +0200

    add path and name to contracts and business_entity_type to Templates

--- src/contracts/models.py
+++ src/contracts/models.py
@@ -3,6 +3,8 @@ from django.utils.translation import gettext_lazy as _
 
 
 class Contracts(models.Model):
+    name = models.CharField(max_length=256, blank=True, null=True)
+    path = models.FileField(upload_to='uploads/', blank=True, null=True, max_length=256)
     business_entities = models.ForeignKey(
         'business_entities.BusinessEntities',
         on_delete=models.SET_NULL,
@@ -22,19 +24,20 @@ class Contracts(models.Model):
 
 
 class Templates(models.Model):
-    class TypeEnum(models.TextChoices):
-        ATP = 'ATP', _('АТП')
-        OFFICE = 'OFFICE', _('Офіс')
+    class BusinessEntityTypeEnum(models.TextChoices):
+        TOV = "TOV", _("ТОВ")
+        FOP = "FOP", _("ФОП")
+
+    business_entity_type = models.CharField(
+        max_length=3,
+        choices=BusinessEntityTypeEnum.choices,
+        default=BusinessEntityTypeEnum.FOP,
+        blank=True,
+        null=True
+    )
 
     name = models.CharField(blank=True, null=True, max_length=100)
-    type = models.CharField(
-        choices=TypeEnum.choices,
-        default=TypeEnum.ATP,
-        blank=True,
-        null=True,
-        max_length=100
-    )
-    path = models.CharField(blank=True, null=True, max_length=100)
+    path = models.FileField(upload_to='uploads/templates/', blank=True, null=True)
 
     def __str__(self):
         return self.name
