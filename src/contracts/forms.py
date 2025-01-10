355bcf6514dbffad2ea840d0f8a90ef592c447ae
Ivan Berestianyi <berestvania@gmail.com>
2025-01-10 16:51:12 +0200

    add contract form

---
+++ src/contracts/forms.py
@@ -0,0 +1,58 @@
+from django import forms
+from datetime import date, timedelta
+
+from contracts.models import Contracts, Templates
+
+
+class ContractTimeRangeForm(forms.Form):
+    start_date = forms.DateField(
+        label="Час початку договору",
+        initial=date.today,
+        widget=forms.DateInput(attrs={"type": "date"}),
+        required=True
+    )
+    end_date = forms.DateField(
+        label="Час закінчення договору",
+        initial=lambda: date.today() + timedelta(days=365),
+        widget=forms.DateInput(attrs={"type": "date"}),
+        required=True
+    )
+
+    class Meta:
+        model = Contracts
+        fields = [
+            'start_date',
+            'end_date'
+        ]
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        for field_name, field in self.fields.items():
+            if 'class' not in field.widget.attrs:
+                field.widget.attrs['class'] = 'form-control'
+
+
+class TemplatesForm(forms.ModelForm):
+    path = forms.FileField(
+        label="Завантажте файл",
+        widget=forms.FileInput(attrs={'type': 'file'}),
+        required=True
+    )
+
+    class Meta:
+        model = Templates
+        fields = ['business_entity_type', 'name', 'path']
+        labels = {
+            'business_entity_type': 'Тип СГ',
+            'name': 'Назва шаблону',
+        }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        for field_name, field in self.fields.items():
+            if 'class' not in field.widget.attrs:
+                field.widget.attrs['class'] = 'form-control'
+
+
