2cfc75f6d12e7352c67d02ab70b385444732d76e
Ivan Berestianyi <berestvania@gmail.com>
2025-01-10 14:42:42 +0200

    add path for contracts

--- src/contracts/urls.py
+++ src/contracts/urls.py
@@ -5,4 +5,14 @@ app_name = 'contracts'
 
 urlpatterns = [
     path('templates', views.ContractTemplatesListView.as_view(), name='templates'),
+    path('search-form/<int:business_entity_id>/', views.create_contract_templates_search_form,
+         name='create_contract_templates_search_form'),
+    path('search/<int:business_entity_id>/', views.search_contract_templates, name='search_contract_templates'),
+    path('create-detail/<int:business_entity_id>/', views.create_document_detail_list,
+         name='create_document_detail_list'),
+    path('create/<int:business_entity_id>/<int:template_id>', views.create_contract, name='create_contract'),
+    path('templates/create/', views.create_template, name='create_template'),
+    path('templates/update/<int:template_id>/', views.update_template, name='update_template'),
+    path('templates/download/<int:pk>/', views.TemplatesDownloadView.as_view(), name='templates_download'),
+    path('redirect-to-templates/', views.redirect_to_templates_list, name='redirect_to_templates_list'),
 ]
