bc2ffe351bfc0f9a26a0a0401391025635c396ac
Ivan Berestianyi <berestvania@gmail.com>
2025-01-14 00:37:49 +0200

    code refactoring

--- src/business_entities/views.py
+++ src/business_entities/views.py
@@ -1,89 +1,101 @@
-from django.db.models import Count
+from django.core.paginator import Paginator
+from django.db.models import Q
 from django.http import HttpResponse
-from django.shortcuts import render, redirect, get_object_or_404
+from django.shortcuts import render
 from django.urls import reverse
-from django.views.generic import ListView
 
 from banks.forms import BankDetailForm
 from contracts.models import Contracts
 from .forms import FOPCreateForm, TOVCreateForm, FOPDetailForm, TOVDetailForm, TOVUpdateForm, \
     FOPUpdateForm
 from .models import BusinessEntities
-from vehicles.models import Vehicles, VehicleLicences
+from vehicles.models import Vehicles
 
 
-class BusinessEntitiesListView(ListView):
-    model = BusinessEntities
-    template_name = 'business_entities/list.html'
-    context_object_name = 'business_entities'
-    paginate_by = 10
+def business_entities_list(request):
+    business_entities = BusinessEntities.objects.all()
+    business_entities_choices = BusinessEntities.BusinessEntitiesEnum
 
+    search_query = request.GET.get('searchBusinessEntity', '').strip()
+    entity_type = request.GET.get('selectBusinessEntity', '')
+    sort_order = request.GET.get('selectDate', 'created_at').strip()
+    page_number = request.GET.get('page')
 
-def create_business_entity(request):
-    business_entity_form = FOPCreateForm()
-    return render(
-        request,
-        'business_entities/create.html',
-        {'business_entity_form': business_entity_form}
-    )
+    if search_query:
+        business_entities = business_entities.filter(
+            Q(edrpou__icontains=search_query) |
+            Q(company_name__icontains=search_query) |
+            Q(director_name__icontains=search_query) |
+            Q(address__icontains=search_query) |
+            Q(email__icontains=search_query) |
+            Q(phone__icontains=search_query) |
+            Q(iban__icontains=search_query)
+        )
+
+    if entity_type:
+        business_entities = business_entities.filter(
+            business_entity=entity_type
+        )
+
+    business_entities = business_entities.order_by(sort_order)
+    paginator = Paginator(business_entities, 2)
+    page_obj = paginator.get_page(page_number)
+
+    context = {
+        'business_entities': page_obj,
+        'business_entities_choices': business_entities_choices,
+        'current_query': request.GET,
+    }
+
+    if request.headers.get("HX-Request") == "true":
+        return render(request, 'business_entities/partials/list_fields.html', context)
+
+    return render(request, 'business_entities/list.html', context)
 
 
 def create_fop_form(request):
-    business_entity_form = FOPCreateForm()
-    return render(
-        request,
-        'business_entities/partials/create_fop_form.html',
-        {'business_entity_form': business_entity_form}
-    )
-
-
-def submit_fop_form(request):
     fop_form = FOPCreateForm(request.POST or None)
+    if request.method == 'POST':
 
-    if fop_form.is_valid():
-        fop = fop_form.save(commit=False)
-        fop.business_entity = BusinessEntities.BusinessEntitiesEnum.FOP
-        fop.save()
+        if fop_form.is_valid():
+            fop = fop_form.save(commit=False)
+            fop.business_entity = BusinessEntities.BusinessEntitiesEnum.FOP
+            fop.save()
 
-        response = HttpResponse("")
-        redirect_url = reverse("business_entity_detail", kwargs={"business_entity_id": fop.id})
-        response["HX-Redirect"] = redirect_url
-        return response
+            response = HttpResponse("")
+            redirect_url = reverse("business_entities:business_entity_detail", kwargs={"business_entity_id": fop.id})
+            response["HX-Redirect"] = redirect_url
+            return response
 
-    return render(
-        request,
-        'business_entities/partials/create_fop_form.html',
-        {'business_entity_form': fop_form}
-    )
+    context = {
+        'business_entity_form': fop_form,
+        'action': 'create_fop'
+    }
+
+    if request.headers.get("HX-Request") == "true":
+        return render(request, 'business_entities/partials/create_form.html', context)
+
+    return render(request, 'business_entities/create.html', context)
 
 
 def create_tov_form(request):
-    business_entity_form = TOVCreateForm()
-    return render(
-        request,
-        'business_entities/partials/create_tov_form.html',
-        {'business_entity_form': business_entity_form}
-    )
-
-
-def submit_tov_form(request):
     tov_form = TOVCreateForm(request.POST or None)
 
-    if tov_form.is_valid():
-        tov = tov_form.save(commit=False)
-        tov.business_entity = BusinessEntities.BusinessEntitiesEnum.TOV
-        tov.save()
+    if request.method == 'POST':
+        if tov_form.is_valid():
+            tov = tov_form.save(commit=False)
+            tov.business_entity = BusinessEntities.BusinessEntitiesEnum.TOV
+            tov.save()
 
-        response = HttpResponse("")
-        redirect_url = reverse("business_entity_detail", kwargs={"business_entity_id": tov.id})
-        response["HX-Redirect"] = redirect_url
-        return response
+            response = HttpResponse("")
+            redirect_url = reverse("business_entities:business_entity_detail", kwargs={"business_entity_id": tov.id})
+            response["HX-Redirect"] = redirect_url
+            return response
 
-    return render(
-        request,
-        'business_entities/partials/create_tov_form.html',
-        {'business_entity_form': tov_form}
-    )
+    return render(request, 'business_entities/partials/create_form.html', {
+        'business_entity_form': tov_form,
+        'action': 'create_tov'
+    })
 
 
 def business_entity_detail(request, business_entity_id):
@@ -107,42 +119,25 @@ def business_entity_detail(request, business_entity_id):
         'business_entity': business_entity,
         'vehicles_with_entities': vehicles_with_entities,
         'bank_form': bank_form,
-        'contracts': contracts
+        'contracts': contracts,
     }
     return render(request, 'business_entities/detail.html', context)
 
 
-def create_business_entity_detail_form(request, business_entity_id):
+def business_entity_detail_form(request, business_entity_id):
     business_entity = BusinessEntities.objects.get(pk=business_entity_id)
     if business_entity.business_entity == BusinessEntities.BusinessEntitiesEnum.FOP:
         business_entity_form = FOPDetailForm(instance=business_entity)
     else:
         business_entity_form = TOVDetailForm(instance=business_entity)
 
-    context = {
+    return render(request, 'business_entities/partials/update_and_detail_form.html', {
         'business_entity_form': business_entity_form,
         'business_entity': business_entity,
-    }
-
-    return render(request, 'business_entities/partials/detail_form.html', context)
+    })
 
 
-def create_business_entity_update_form(request, business_entity_id):
-    business_entity = BusinessEntities.objects.get(pk=business_entity_id)
-    if business_entity.business_entity == BusinessEntities.BusinessEntitiesEnum.FOP:
-        business_entity_form = FOPUpdateForm(instance=business_entity)
-    else:
-        business_entity_form = TOVUpdateForm(instance=business_entity)
-
-    context = {
-        'business_entity_form': business_entity_form,
-        'business_entity': business_entity,
-    }
-
-    return render(request, 'business_entities/partials/update_form.html', context)
-
-
-def submit_business_entity_update_form(request, business_entity_id):
+def business_entity_update_form(request, business_entity_id):
     business_entity = BusinessEntities.objects.get(pk=business_entity_id)
     if business_entity.business_entity == BusinessEntities.BusinessEntitiesEnum.FOP:
         business_entity_form = FOPUpdateForm(request.POST or None, instance=business_entity)
@@ -156,19 +151,15 @@ def submit_business_entity_update_form(request, business_entity_id):
                 updated_business_entity_form = FOPDetailForm(instance=updated_business_entity)
             else:
                 updated_business_entity_form = TOVDetailForm(instance=updated_business_entity)
-            return render(request, 'business_entities/partials/detail_form.html', {
+            return render(request, 'business_entities/partials/update_and_detail_form.html', {
                 'business_entity_form': updated_business_entity_form,
                 'business_entity': updated_business_entity,
             })
-        else:
-            return render(request, 'business_entities/partials/update_form.html', {
-                'business_entity_form': business_entity_form,
-                'business_entity': business_entity,
-            })
 
-    return render(request, 'business_entities/partials/update_form.html', {
+    return render(request, 'business_entities/partials/update_and_detail_form.html', {
         'business_entity_form': business_entity_form,
         'business_entity': business_entity,
+        'action': 'update_business_entity'
     })
 
 
@@ -177,106 +168,6 @@ def delete_business_entity(request, business_entity_id):
     if request.method == 'POST':
         business_entity.delete()
         response = HttpResponse("")
-        redirect_url = reverse("business_entities")
+        redirect_url = reverse("business_entities:business_entities")
         response["HX-Redirect"] = redirect_url
         return response
-
-
-def create_search_vehicle_form(request, business_entity_id):
-    business_entity = BusinessEntities.objects.get(pk=business_entity_id)
-    vehicles_without_entities = (
-        Vehicles.objects
-        .annotate(num_entities=Count('vehiclelicences__business_entities'))
-        .filter(num_entities=0)
-    )
-
-    return render(
-        request,
-        'business_entities/partials/vehicle_search_form.html',
-        {
-            'vehicles_without_entities': vehicles_without_entities,
-            'business_entity': business_entity
-        }
-    )
-
-
-def search_vehicles_without_entities(request, business_entity_id):
-    business_entity = BusinessEntities.objects.get(pk=business_entity_id)
-    query = request.GET.get('q', '')
-
-    vehicles_without_entities = (
-        Vehicles.objects
-        .annotate(num_entities=Count('vehiclelicences__business_entities'))
-        .filter(num_entities=0)
-    )
-
-    if query:
-        vehicles_without_entities = vehicles_without_entities.filter(
-            number__icontains=query
-        )
-
-    return render(
-        request,
-        'business_entities/partials/vehicle_search_list.html',
-        {
-            'vehicles_without_entities': vehicles_without_entities,
-            'query': query,
-            'business_entity': business_entity,
-        }
-    )
-
-
-def add_vehicle_to_business_entity(request, business_entity_id, vehicle_id):
-    business_entity = get_object_or_404(BusinessEntities, id=business_entity_id)
-    vehicle = get_object_or_404(Vehicles, id=vehicle_id)
-    vehicle_licence = get_object_or_404(VehicleLicences, vehicle=vehicle)
-    vehicle_licence.business_entities.add(business_entity)
-    vehicle_licence.save()
-
-    vehicles_with_entities = (
-        Vehicles.objects
-        .filter(vehiclelicences__business_entities=business_entity)
-        .distinct()
-    )
-
-    context = {
-        'vehicles_with_entities': vehicles_with_entities,
-        'business_entity': business_entity,
-    }
-
-    return render(request, 'business_entities/vehicle_list.html', context)
-
-
-def remove_vehicle_from_business_entity(request, business_entity_id, vehicle_id):
-    business_entity = get_object_or_404(BusinessEntities, id=business_entity_id)
-    vehicle = get_object_or_404(Vehicles, id=vehicle_id)
-    vehicle_licence = get_object_or_404(VehicleLicences, vehicle=vehicle)
-    vehicle_licence.business_entities.remove(business_entity)
-    vehicle_licence.save()
-
-    vehicles_with_entities = (
-        Vehicles.objects
-        .filter(vehiclelicences__business_entities=business_entity)
-        .distinct()
-    )
-    context = {
-        'vehicles_with_entities': vehicles_with_entities,
-        'business_entity': business_entity,
-
-    }
-
-    return render(request, 'business_entities/vehicle_list.html', context)
-
-
-def vehicles_in_business_entity(request, business_entity_id):
-    business_entity = BusinessEntities.objects.get(pk=business_entity_id)
-    vehicles_with_entities = (
-        Vehicles.objects
-        .filter(vehiclelicences__business_entities=business_entity)
-        .distinct()
-    )
-    context = {
-        'vehicles_with_entities': vehicles_with_entities,
-        'business_entity': business_entity,
-    }
-    return render(request, 'business_entities/vehicle_list.html', context)
