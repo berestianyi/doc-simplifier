84312545a55309b81024a415ca6aff569daf2bcc
Ivan Berestianyi <berestvania@gmail.com>
2025-02-04 02:27:50 +0200

    add contract templates urls

---
+++ src/contracts/urls/templates_urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from contracts import views
+
+app_name = 'templates'
+
+urlpatterns = [
+    path('', views.ContractTemplatesListView.as_view(), name='list'),
+    path('create/', views.TemplateCreateView.as_view(), name='create'),
+    path('update/<int:template_id>/', views.TemplateUpdateView.as_view(), name='update'),
+    path('download/<int:template_id>/', views.TemplatesDownloadView.as_view(), name='download'),
+]
