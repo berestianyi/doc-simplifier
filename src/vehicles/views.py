3bd1c6bed564cf4a2948d0d550847f837e831104
Ivan Berestianyi <berestvania@gmail.com>
2025-02-04 02:31:43 +0200

    fixed vehicle view

--- src/vehicles/views.py
+++ src/vehicles/views.py
@@ -1,12 +1,10 @@
 from typing import List
-from django.db.models import Count
 from django.http import HttpResponse
-from django.shortcuts import render, redirect, get_object_or_404
+from django.shortcuts import render, redirect
 from django.urls import reverse
-from django.views.generic import ListView, View
+from django.views.generic import ListView, View, CreateView, TemplateView, UpdateView
 
-from business_entities.models import BusinessEntities
-from business_entities.views import HtmxTemplateMixin, SortOrderMixin, SearchMixin, BusinessEntityMixin
+from business_entities.mixins import HtmxMixin, SearchMixin, SortOrderMixin, BusinessEntityMixin
 from .forms import (
     VehiclesCreateForm,
     VehiclesUpdateForm,
@@ -15,35 +13,14 @@ from .forms import (
     VehicleLicencesDetailForm,
     VehicleLicencesCreateForm
 )
-from vehicles.models import Vehicles, VehicleLicences
+from .models import Vehicles
+from .mixins import VehicleMixin
 
 
-class VehicleMixin:
-
-    @classmethod
-    def get_vehicle_and_licence_objects(cls, vehicle_id):
-        vehicle = get_object_or_404(Vehicles, id=vehicle_id)
-        licence = get_object_or_404(VehicleLicences, vehicle=vehicle)
-        return vehicle, licence
-
-    @classmethod
-    def vehicles_without_business_entities(cls, business_entity):
-        owned_vehicle_ids = VehicleLicences.objects.filter(
-            business_entities=business_entity
-        ).values_list('vehicle_id', flat=True)
-        unowned_vehicles = Vehicles.objects.exclude(id__in=owned_vehicle_ids)
-
-        return unowned_vehicles
-
-    @classmethod
-    def vehicles_with_business_entity(cls, business_entity: BusinessEntities):
-        return Vehicles.objects.filter(vehiclelicences__business_entities=business_entity).distinct()
-
-
-class VehiclesListView(HtmxTemplateMixin, SortOrderMixin, SearchMixin, ListView):
+class VehiclesListView(HtmxMixin, SortOrderMixin, SearchMixin, ListView):
     model = Vehicles
     template_name = 'vehicles/list.html'
-    htmx_template_name = 'vehicles/partials/list_fields.html'
+    htmx_template_name = 'vehicles/partials/_fields.html'
     context_object_name = 'vehicles'
     paginate_by = 10
     search_param_name = 'searchVehicles'
@@ -68,129 +45,114 @@ class VehiclesListView(HtmxTemplateMixin, SortOrderMixin, SearchMixin, ListView)
         return queryset
 
 
-class CreateVehicleView(View):
-    template_name = 'vehicles/CRUD.html'
+class CreateVehicleView(BusinessEntityMixin, CreateView):
+    model = Vehicles
+    form_class = VehiclesCreateForm
+    template_name = 'vehicles/create.html'
 
-    def get(self, request, *args, **kwargs):
-        vehicle_form = VehiclesCreateForm()
-        licence_form = VehicleLicencesCreateForm()
-        return render(
-            request,
-            self.template_name,
-            {
-                'vehicle_form': vehicle_form,
-                'licence_form': licence_form,
-                'action': 'create'
-            }
-        )
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['vehicle_form'] = context.pop('form', None)
+        if 'licence_form' not in context:
+            context['licence_form'] = VehicleLicencesCreateForm(self.request.POST or None)
+        context['business_entity'] = self.business_entity
+        return context
 
-    def post(self, request, *args, **kwargs):
-        vehicle_form = VehiclesCreateForm(request.POST)
-        licence_form = VehicleLicencesCreateForm(request.POST)
-
-        if vehicle_form.is_valid() and licence_form.is_valid():
-            vehicle = vehicle_form.save(commit=True)
+    def form_valid(self, form):
+        context = self.get_context_data()
+        vehicle_form = context.get('vehicle_form')
+        licence_form = context.get('licence_form')
+        if licence_form.is_valid():
+            self.object = vehicle_form.save()
             licence = licence_form.save(commit=False)
-            licence.vehicle = vehicle
+            licence.vehicle = self.object
             licence.save()
-            return redirect('vehicles:detail', vehicle_id=vehicle.id)
-
-        return render(
-            request,
-            self.template_name,
-            {
-                'vehicle_form': vehicle_form,
-                'licence_form': licence_form,
-                'action': 'create'
-            }
-        )
-
-
-class VehicleAndLicenceDetailView(VehicleMixin, HtmxTemplateMixin, View):
-    template_name = 'vehicles/CRUD.html'
-    htmx_template_name = 'vehicles/partials/CRUD_form.html'
-
-    def get(self, request, vehicle_id):
-        vehicle, licence = self.get_vehicle_and_licence_objects(vehicle_id)
-
-        context = {
-            'vehicle_form': VehiclesDetailForm(instance=vehicle),
-            'licence_form': VehicleLicencesDetailForm(instance=licence),
-            'vehicle': vehicle,
-            'licence': licence,
-            'action': 'detail'
-        }
-        templates = self.get_template_names()
-        selected_template = templates[0] if templates else self.template_name
-
-        return render(request, selected_template, context)
-
-
-class VehicleAndLicenceUpdateView(VehicleMixin, View):
-    template_name = 'vehicles/partials/CRUD_form.html'
-
-    def get(self, request, vehicle_id):
-        vehicle, licence = self.get_vehicle_and_licence_objects(vehicle_id)
-        context = {
-            'vehicle_form': VehiclesUpdateForm(instance=vehicle),
-            'licence_form': VehicleLicencesUpdateForm(instance=licence),
-            'vehicle': vehicle,
-            'action': 'update'
-        }
-        return render(request, self.template_name, context)
-
-    def post(self, request, vehicle_id):
-        vehicle, licence = self.get_vehicle_and_licence_objects(vehicle_id)
-        licence_update_form = VehicleLicencesUpdateForm(request.POST, instance=licence)
-        vehicle_update_form = VehiclesUpdateForm(request.POST, instance=vehicle)
-
-        if licence_update_form.is_valid() and vehicle_update_form.is_valid():
-            updated_licence = licence_update_form.save()
-            updated_vehicle = vehicle_update_form.save()
-            return render(request, self.template_name, {
-                'licence_form': VehicleLicencesDetailForm(instance=updated_licence),
-                'vehicle_form': VehiclesDetailForm(instance=updated_vehicle),
-                'vehicle': updated_vehicle,
-                'action': 'detail'
-            })
+            if self.business_entity:
+                licence.business_entities.add(self.business_entity)
+                licence.save()
+                return redirect(
+                    'business_entities:detail',
+                    business_entity_id=self.business_entity.id
+                )
+            return redirect('vehicles:detail', vehicle_id=self.object.id)
         else:
-            return render(request, self.template_name, {
-                'licence_form': licence_update_form,
-                'vehicle_form': vehicle_update_form,
-                'vehicle': vehicle,
-                'action': 'update'
-            })
+            return self.form_invalid(vehicle_form)
+
+    def form_invalid(self, form):
+        context = self.get_context_data(form=form)
+        return self.render_to_response(context)
 
 
-class VehicleAndLicenceDeleteView(VehicleMixin, View):
-    def post(self, request, vehicle_id):
-        vehicle, licence = self.get_vehicle_and_licence_objects(vehicle_id)
-        vehicle.delete()
-        licence.delete()
-        response = HttpResponse("")
-        redirect_url = reverse("vehicles:list")
-        response["HX-Redirect"] = redirect_url
-        return response
+class VehicleAndLicenceDetailView(VehicleMixin, HtmxMixin, TemplateView):
+    template_name = 'vehicles/detail.html'
+    htmx_template_name = 'vehicles/partials/forms/_detail.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context.update({
+            'vehicle_form': VehiclesDetailForm(instance=self.vehicle),
+            'licence_form': VehicleLicencesDetailForm(instance=self.vehicle_licence),
+            'vehicle': self.vehicle,
+            'licence': self.vehicle_licence,
+        })
+        return context
 
 
-class VehicleRedirectToDetailView(View):
+class VehicleAndLicenceUpdateView(VehicleMixin, UpdateView):
+    model = Vehicles
+    form_class = VehiclesUpdateForm
+    template_name = 'vehicles/partials/forms/_update.html'
+    detail_template_name = 'vehicles/partials/forms/_detail.html'
+
+    def get_object(self, queryset=None):
+        return self.vehicle
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['vehicle_form'] = context.pop('form', None)
+        context['licence_form'] = VehicleLicencesUpdateForm(
+            self.request.POST or None,
+            instance=self.vehicle_licence
+        )
+        context['vehicle'] = self.vehicle
+        return context
+
+    def form_valid(self, form):
+        context = self.get_context_data()
+        vehicle_form = context.get('vehicle_form')
+        licence_form = context.get('licence_form')
+
+        if licence_form.is_valid():
+            updated_vehicle = vehicle_form.save()
+            updated_licence = licence_form.save()
+            context = {
+                'vehicle_form': VehiclesDetailForm(instance=updated_vehicle),
+                'licence_form': VehicleLicencesDetailForm(instance=updated_licence),
+                'vehicle': updated_vehicle,
+            }
+            return render(self.request, self.detail_template_name, context)
+        else:
+            return self.form_invalid(vehicle_form)
+
+    def form_invalid(self, form):
+        context = self.get_context_data(form=form)
+        return self.render_to_response(context)
+
+
+class VehicleAndLicenceDeleteView(BusinessEntityMixin, VehicleMixin, View):
+    def post(self, request,  *args, **kwargs):
+        self.vehicle_licence.delete()
+        return redirect('vehicles:list')
+
+
+class VehicleRedirectToDetailView(HtmxMixin, View):
     def get(self, request, vehicle_id, *args, **kwargs):
-        response = HttpResponse("")
-        redirect_url = reverse("vehicles:detail", kwargs={'vehicle_id': vehicle_id})
-        response["HX-Redirect"] = redirect_url
-        return response
-
-
-class VehicleRedirectToCreateView(View):
-    def get(self, request, *args, **kwargs):
-        response = HttpResponse("")
-        redirect_url = reverse("vehicles:create")
-        response["HX-Redirect"] = redirect_url
+        response = self.htmx_redirect("vehicles:detail", kwargs={'vehicle_id': vehicle_id})
         return response
 
 
 class CreateSearchVehicleFormView(BusinessEntityMixin, VehicleMixin, ListView):
-    template_name = 'vehicles/partials/search_form.html'
+    template_name = 'vehicles/partials/forms/_search.html'
     context_object_name = 'vehicles_without_entities'
     paginate_by = 5
 
@@ -206,11 +168,11 @@ class CreateSearchVehicleFormView(BusinessEntityMixin, VehicleMixin, ListView):
 
 class SearchVehiclesWithoutEntitiesListView(VehicleMixin, BusinessEntityMixin, SearchMixin, ListView):
     model = Vehicles
-    template_name = 'vehicles/partials/search_list.html'
+    template_name = 'vehicles/partials/lists/_search.html'
     context_object_name = 'vehicles_without_entities'
     paginate_by = 5
     search_param_name = 'searchVehicles'
-    search_fields: List[str] = ['number']
+    search_fields: List[str] = ['number', 'model', 'brand']
 
     def get_queryset(self):
         queryset = self.vehicles_without_business_entities(self.business_entity)
@@ -225,70 +187,57 @@ class SearchVehiclesWithoutEntitiesListView(VehicleMixin, BusinessEntityMixin, S
         return context
 
 
-class AddVehicleToBusinessEntityView(VehicleMixin, View):
-    template_name = 'vehicles/detail_list.html'
+class AddVehicleToBusinessEntityView(BusinessEntityMixin, VehicleMixin, View):
+    template_name = 'vehicles/partials/lists/_detail.html'
 
-    def post(self, request, business_entity_id, vehicle_id, *args, **kwargs):
-        business_entity = get_object_or_404(BusinessEntities, id=business_entity_id)
-        vehicle, licence = self.get_vehicle_and_licence_objects(vehicle_id)
+    def post(self, request, *args, **kwargs):
+        self.vehicle_licence.business_entities.add(self.business_entity)
+        self.vehicle_licence.save()
 
-        licence.business_entities.add(business_entity)
-        licence.save()
-
-        vehicles_with_entities = self.vehicles_with_business_entity(business_entity)
+        vehicles_with_entities = self.vehicles_with_business_entity(self.business_entity)
 
         context = {
             'vehicles_with_entities': vehicles_with_entities,
-            'business_entity': business_entity,
+            'business_entity': self.business_entity,
         }
         return render(request, self.template_name, context)
 
 
-class RemoveVehicleFromBusinessEntityView(VehicleMixin, View):
-    template_name = 'vehicles/detail_list.html'
+class RemoveVehicleFromBusinessEntityView(BusinessEntityMixin, VehicleMixin, View):
+    template_name = 'vehicles/partials/lists/_detail.html'
 
-    def post(self, request, business_entity_id, vehicle_id, *args, **kwargs):
-        business_entity = get_object_or_404(BusinessEntities, id=business_entity_id)
-        vehicle, licence = self.get_vehicle_and_licence_objects(vehicle_id)
+    def post(self, request, *args, **kwargs):
+        self.vehicle_licence.business_entities.remove(self.business_entity)
+        self.vehicle_licence.save()
 
-        licence.business_entities.remove(business_entity)
-        licence.save()
-
-        vehicles_with_entities = self.vehicles_with_business_entity(business_entity)
+        vehicles_with_entities = self.vehicles_with_business_entity(self.business_entity)
 
         context = {
             'vehicles_with_entities': vehicles_with_entities,
-            'business_entity': business_entity,
+            'business_entity': self.business_entity,
         }
         return render(request, self.template_name, context)
 
 
-class VehiclesInBusinessEntityListView(VehicleMixin, ListView):
+class VehiclesInBusinessEntityListView(BusinessEntityMixin, VehicleMixin, ListView):
     model = Vehicles
-    template_name = 'vehicles/detail_list.html'
+    template_name = 'vehicles/partials/lists/_detail.html'
     context_object_name = 'vehicles_with_entities'
 
     def get_queryset(self):
-        business_entity_id = self.kwargs['business_entity_id']
-        business_entity = get_object_or_404(BusinessEntities, pk=business_entity_id)
-        return self.vehicles_with_business_entity(business_entity)
+        return self.vehicles_with_business_entity(self.business_entity)
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        business_entity_id = self.kwargs['business_entity_id']
-        context['business_entity'] = get_object_or_404(BusinessEntities, pk=business_entity_id)
+        context['business_entity'] = self.business_entity
         return context
 
 
-class VehiclesInBusinessEntityView(VehicleMixin, View):
-    template_name = 'vehicles/detail_list.html'
+class VehiclesInBusinessEntityView(BusinessEntityMixin, VehicleMixin, TemplateView):
+    template_name = 'vehicles/partials/lists/_detail.html'
 
-    def get(self, request, business_entity_id, *args, **kwargs):
-        business_entity = get_object_or_404(BusinessEntities, pk=business_entity_id)
-        vehicles_with_entities = self.vehicles_with_business_entity(business_entity)
-
-        context = {
-            'vehicles_with_entities': vehicles_with_entities,
-            'business_entity': business_entity,
-        }
-        return render(request, self.template_name, context)
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['business_entity'] = self.business_entity
+        context['vehicles_with_entities'] = self.vehicles_with_business_entity(self.business_entity)
+        return context
