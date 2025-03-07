904b9aaa0a9b4c496408be4c061e7ea9d7b82c0c
Ivan Berestianyi <berestvania@gmail.com>
2025-03-07 13:48:58 +0200

    update models

--- src/banks/models.py
+++ src/banks/models.py
@@ -3,8 +3,8 @@ from django.utils.translation import gettext_lazy as _
 
 
 class Bank(models.Model):
-    name = models.CharField(_("Bank Name"), max_length=200, blank=True, null=True)
-    mfo = models.CharField(_("MFO"), blank=True, null=True)
+    name = models.CharField(_("Bank Name"), max_length=200, blank=True, default="")
+    mfo = models.CharField(_("MFO"), blank=True, default="")
 
     def __str__(self):
         return f"Bank: {self.name}"
