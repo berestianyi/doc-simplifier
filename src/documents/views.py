64e46d5c88931d9af8e910b337d7399ed8e26eaf
Ivan Berestianyi <berestvania@gmail.com>
2025-03-07 13:47:02 +0200

    update views

--- src/documents/views.py
+++ src/documents/views.py
@@ -1,39 +1,52 @@
-from django.shortcuts import redirect
+from django.http import JsonResponse
+from django.shortcuts import render
 from django.views import View
-from django.views.generic import FormView
 
-from business_entities.models import BusinessEntitiesEnum, BusinessEntities
+from business_entities.forms import FOPCreateForm, TOVCreateForm
 from .forms import PDFUploadForm
 from .mixins import DocumentMixin
 from .models import Documents
-from .services import TextExtraction
+from .services.core import DocumentService
 
 
-class PDFUploadView(FormView):
+class PDFUploadView(View):
     template_name = 'documents/pdf_text_extraction.html'
     form_class = PDFUploadForm
 
-    def form_valid(self, form):
-        pdf_file = form.cleaned_data['pdf_file']
-        text_extraction = TextExtraction(pdf_file)
-        data = text_extraction.extract_data()
+    def post(self, request, *args, **kwargs):
+        form = self.form_class(request.POST, request.FILES)
 
-        business_entity_type = (
-            BusinessEntitiesEnum.FOP if text_extraction.is_fop() else BusinessEntitiesEnum.TOV
+        if not form.is_valid():
+            return JsonResponse({
+                'success': False,
+            }, status=400)
+
+        pdf_file = form.files.get('pdf_file')
+        service = DocumentService(pdf_file)
+        data = service.extract_data()
+        if service.is_fop(data.get('edrpou')):
+            form_class = FOPCreateForm
+            template = 'business_entities/partials/forms/_fop_create.html'
+        else:
+            form_class = TOVCreateForm
+            template = 'business_entities/partials/forms/_tov_create.html'
+
+        create_form = form_class(initial={
+            'edrpou': data.get('edrpou'),
+            'company_name': data.get('company_name'),
+            'director_name': data.get('director_name'),
+            'address': data.get('address'),
+            'phone': data.get('phone'),
+            'email': data.get('email'),
+        })
+
+        return render(
+            request,
+            template,
+            {
+                'business_entity_form': create_form
+            }
         )
-
-        address = TextExtraction.address_handler(data.get("address"))
-
-        business_entity = BusinessEntities.objects.create(
-            business_entity=business_entity_type,
-            edrpou=data.get('edrpou'),
-            company_name=data.get('company_name'),
-            director_name=data.get('director_name'),
-            address=address,
-            email=data.get('email'),
-            phone=data.get('phone'),
-        )
-        return redirect('business_entities:business_entity_detail', business_entity_id=business_entity.id)
 
 
 class DocumentsDownloadView(DocumentMixin, View):
